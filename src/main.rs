4a4de9e2efe4f3f77da2c2858a22e05cee000479
Eugene P <eupn@protonmail.com>
2018-12-29 14:47:01 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+
+// Import our procedural macros crate
+extern crate rust_procmacro_quickstart_template;
+
+// Import procedural macro attribute
+use rust_procmacro_quickstart_template::{my_attribute, MyDerive, my_macro};
+
+#[my_attribute]
+struct TestAttrStruct {}
+
+#[my_attribute]
+fn test_attr_function() {}
+
+#[derive(MyDerive)]
+struct TestDeriveStruct {}
+
+#[derive(MyDerive)]
+enum TestDeriveEnum {}
+
+my_macro!(2 + 2);
+
+pub fn main() {
+    // my_macro!(2 + 2);
+
+    // ^ as of rust 1.30, this gives an error[E0658]: procedural macros cannot be expanded to statements (see issue #54727)
+    // This error might be fixed in future, although, there's workaround exists: https://github.com/dtolnay/proc-macro-hack
+}
